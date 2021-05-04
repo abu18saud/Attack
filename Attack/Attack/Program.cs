6025c740f78933fc0d62a8d8ac09a9f5ebea7b98
Abdullah Alsalem <abdullahalsalem@MacBook-Pro-alkhas-b-Abdullah.local>
2021-05-04 05:46:40 +0300

    Done
    Done

---
+++ Attack/Attack/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Attack
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Unit unit1 = new Unit();
+            Unit musketeer = new Musketeer();
+            Unit magician = new Magician();
+
+            unit1.Attack();
+            musketeer.Attack();
+            magician.Attack();
+        }
+        class Unit
+        {
+            public virtual void Attack()
+            {
+                Console.WriteLine("Using sword!");
+            }
+        }
+        /*derive the class from Unit class
+and override Attack() method*/
+        class Musketeer: Unit
+        {
+            public override void Attack()
+            {
+                Console.WriteLine("Using musket!");
+            }
+        }
+        /*derive the class from Unit class
+and override Attack() method*/
+        class Magician: Unit
+        {
+            public override void Attack()
+            {
+                Console.WriteLine("Using magic!");
+            }
+        }
+    }
+}
